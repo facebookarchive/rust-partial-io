abe7888ae1bd91a14470f36a3fc7912ce325c5b5
sunshowers <rain@sunshowers.io>
2021-01-20 21:52:28 +0000

    Deploying to gh-pages from  @ 486f1ef4f54ed6590316bd3f3a5f29c46d23a7ce 🚀

--- rustdoc/implementors/rand/trait.SeedableRng.js
+++ rustdoc/implementors/rand/trait.SeedableRng.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rand"] = [{"text":"impl SeedableRng for StdRng","synthetic":false,"types":[]}];
+implementors["rand"] = [{"text":"impl SeedableRng for SmallRng","synthetic":false,"types":[]},{"text":"impl SeedableRng for StdRng","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
